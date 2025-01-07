2942c03a703be6e198631aa131552bdff92d679e
41323150 <yue950116@gmail.com>
2025-01-07 09:43:33 +0000

    修改w10題目2影片

--- test2.py
+++ test2.py
@@ -1,2 +1,11 @@
-sum = 1+2+3+4+5+6+7+8+9+10
-print(sum)
+'''
+Python 的 keywords 為保留字, 不可作為變數名稱
+'''
+import keyword
+
+# 取得 Python 的 keywords
+keywords = keyword.kwlist
+
+print(keywords)
+and = [5,[4,3],2,1]
+print(and[2])
